7719280dc91f9824244375e0eaba3bbfc97c26a8
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-09-01 14:25:51 +0000

    ~p99

---
+++ 3times1.rb
@@ -0,0 +1,17 @@
+3.times do
+  puts "アイスを買って帰ろう"
+end 
+
+3.times {
+  puts "カフェラテ"
+}
+
+3.times do puts "モカ" end 
+3.times {puts "ラテ"}
+
+3.times do 
+  puts "モカ"
+  puts "ください"
+end 
+
+3.times {puts "ラテください"}
---
+++ while.rb
@@ -0,0 +1,10 @@
+biscuit = 0
+while biscuit < 2
+  puts "ポケットを叩くとビスケットが#{biscuit}つ"
+  biscuit = biscuit +1
+end 
+
+while biscuit < 5
+  puts "箱に入っているビスケットが#{biscuit}つ"
+  biscuit += 2
+end
